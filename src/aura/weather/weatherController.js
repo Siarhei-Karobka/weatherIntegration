e25e942e7d1c2665abcf59d97a54bd1e548254a8
okborok <okborok1@gmail.com>
2019-09-23 13:59:17 +0300

    add error message

--- src/aura/weather/weatherController.js
+++ src/aura/weather/weatherController.js
@@ -1,15 +1,6 @@
-/**
-    * Created by OK on 16.09.2019.
-    */
 ({
     doInit: function (component, event, helper) {
-        helper.init(component);
+        var days = event.getSource().get('v.value');
+        helper.init(component, days);
     }
-    //onKeyDown: function (){
-    //    var char = /["a-zA-Z]/;
-    //    var val = String.fromCharCode(e.which);
-    //    var test = char.test(val);
-    //
-    //    if(!test) return false;
-    //}
 });
--- src/aura/weather/weatherHelper.js
+++ src/aura/weather/weatherHelper.js
@@ -1,19 +1,26 @@
-/**
-    * Created by OK on 16.09.2019.
-    */
 ({
-    init: function (component) {
+    init: function (component, days) {
         var action = component.get("c.getWeather");
 
-        action.setParams({"city": component.get('v.choosedCity')});
+        action.setParams({
+            "city": component.get('v.choosedCity'),
+            "days": days
+        });
         action.setCallback(this, function (response) {
             var state = response.getState();
             if (state === "SUCCESS") {
-                component.set("v.cityInfo", response.getReturnValue());
-            } else {
-
-                console.log("Failed with state: " + state);
-                alert('Can\'t find your city!');
+                var responseObj = JSON.parse(response.getReturnValue());
+                if (responseObj != null) {
+                    var cityInfo = {
+                        city_name: responseObj.city_name,
+                        country_code: responseObj.country_code
+                    };
+                    component.set('v.cityData', responseObj.data);
+                    component.set("v.cityInfo", cityInfo);
+                    component.set("v.showErrors", false);
+                } else {
+                    component.set("v.showErrors", true);
+                }
             }
         });
         $A.enqueueAction(action);
